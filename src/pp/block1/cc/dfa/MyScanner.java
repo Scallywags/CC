f3121f18a7e7bc259bfebfe25eb6e2544f7db4b2
Jannyboy11 <jannyboy11@gmail.com>
2016-04-20 11:28:49 +0200

    Scanner implementation

--- src/pp/block1/cc/dfa/MyScanner.java
+++ src/pp/block1/cc/dfa/MyScanner.java
@@ -1,17 +1,33 @@
 package pp.block1.cc.dfa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyScanner implements Scanner {
 
 	@Override
 	public List<String> scan(State dfa, String text) {
+		State start = dfa;
+		List<String> tokens = new ArrayList<>();
 		
-		
-		
-		
-		// TODO Auto-generated method stub
-		return null;
+		while (!text.isEmpty()) {
+			StringBuilder token = new StringBuilder();
+			while (dfa.hasNext(text.charAt(0))) {
+				token.append(text.charAt(0));
+				dfa = dfa.getNext(text.charAt(0));
+				text = text.substring(1);
+				if (text.isEmpty()) {
+					break;
+				}
+			}
+			if (dfa.isAccepting()) {
+				tokens.add(token.toString());
+			} else {
+				return null;
+			}
+			dfa = start;
+		}
+		return tokens;
 	}
 
 }
