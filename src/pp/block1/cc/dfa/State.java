b3bb28a97a5fa58ef2c4cf55600b09b8d49c3fd1
Jannyboy11 <jannyboy11@gmail.com>
2016-04-20 11:40:05 +0200

    LALA DFA

--- src/pp/block1/cc/dfa/State.java
+++ src/pp/block1/cc/dfa/State.java
@@ -97,4 +97,42 @@ public class State {
 			}
 		}
 	}
+	
+	public static final State LA_DFA = new State(0, false);
+	static {
+		State q1 = new State(1, false);
+		State q2 = new State(2, true);
+		State q3 = new State(3, true);
+		State q4 = new State(4, false);
+		State q5 = new State(5, true);
+		State q6 = new State(6, true);
+		State q7 = new State(7, false);
+		State q8 = new State(8, false);
+		State q9 = new State(9, false);
+		State q10 = new State(10, false);
+		State q11 = new State(11, true);
+		
+		LA_DFA.addNext('L', q1);
+		q1.addNext('a', q2);
+		q2.addNext('a', q2);
+		q2.addNext(' ', q3);
+		q3.addNext(' ', q3);
+		q2.addNext('L', q4);
+		q3.addNext('L', q4);
+		q4.addNext('a', q5);
+		q5.addNext('a', q5);
+		q5.addNext(' ', q6);
+		q6.addNext(' ', q6);
+		q5.addNext('L', q7);
+		q6.addNext('L', q7);
+		q7.addNext('a', q8);
+		q8.addNext('a', q8);
+		q8.addNext(' ', q9);
+		q9.addNext(' ', q9);
+		q8.addNext('L', q10);
+		q9.addNext('L', q10);
+		q10.addNext('i', q11);
+		q11.addNext(' ', q11);
+	}
+	
 }
