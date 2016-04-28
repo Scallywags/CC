e1b377df845ddc3dc1b2f593bc7e17a0b5379cab
Jannyboy11 <jannyboy11@gmail.com>
2016-04-28 11:47:00 +0200

    we dont need that listiterator anymore

--- src/pp/block2/cc/ll/MyLLCalc.java
+++ src/pp/block2/cc/ll/MyLLCalc.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.Set;
