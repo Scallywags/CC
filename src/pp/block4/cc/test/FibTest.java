3a242d76c9e7669c210e3367e924a0d216001606
Jannyboy11 <jannyboy11@gmail.com>
2016-05-20 12:11:23 +0200

    Better FibTest

--- src/pp/block4/cc/test/FibTest.java
+++ src/pp/block4/cc/test/FibTest.java
@@ -15,31 +15,54 @@ public class FibTest {
 	
 	@Test
 	public void testRegToReg() throws Exception {
+		for (int n = 2; n < 45; n++) {
+			testFib(n);
+		}
+		System.out.println(testFib(45));
+		System.out.println(testFib(46));
+	}
+	
+	private int testFib(int n) throws Exception {
+		int expected = fib(n);
+		
 		Assembler assembler = Assembler.instance();
 		Program p = assembler.assemble(new File("fibreg.iloc"));
 		Simulator sim = new Simulator(p);
 		Machine vm = sim.getVM();
-		vm.setNum("n", 31);
+		vm.setNum("n", n);
 		vm.setNum("z", 0);
 		sim.run();
 		int fib = vm.getReg("r_z");
-		assertEquals(2178309, fib);
-	}
-	
-	@Test
-	public void testMemToMem() throws Exception {
-		Assembler assembler = Assembler.instance();
-		Program p = assembler.assemble(new File("fibmem.iloc"));
-		Simulator sim = new Simulator(p);
-		Machine vm = sim.getVM();
+		assertEquals(expected, fib);
+		
+		assembler = Assembler.instance();
+		p = assembler.assemble(new File("fibmem.iloc"));
+		sim = new Simulator(p);
+		vm = sim.getVM();
 		vm.setSize(16);
-		vm.setNum("n", 31);
+		vm.setNum("n", n);
 		vm.setNum("x", 0);
 		vm.setNum("y", 4);
 		vm.setNum("z", 8);
 		sim.run();
-		int fib = vm.getReg("r_z");
-		assertEquals(2178309, fib);
+		fib = vm.getReg("r_z");
+		assertEquals(expected, fib);
+		
+		return expected;
+	}
+	
+	private int fib(int n) {
+		int x = 1;
+		int y = 1;
+		int z = 1;
+				
+		while (n > 1) {
+			z = y + x;
+			x = y;
+			y = z;
+			n--;
+		}
+		return z;
 	}
 
 }
